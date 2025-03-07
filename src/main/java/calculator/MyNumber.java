426fd90662d1fb110aaac4145185d3028b6a4942
augho <aughouba@gmail.com>
2025-03-07 17:04:42 +0100

    refactor: MyNumber is now abstract
    It can now be implemented for different types of numbers

--- src/main/java/calculator/MyNumber.java
+++ src/main/java/calculator/MyNumber.java
@@ -3,6 +3,8 @@ package calculator;
 import visitor.CountVisitor;
 import visitor.Visitor;
 
+import java.util.Objects;
+
 /**
  * MyNumber is a concrete class that represents arithmetic numbers,
  * which are a special kind of Expressions, just like operations are.
@@ -10,109 +12,99 @@ import visitor.Visitor;
  * @see Expression
  * @see Operation
  */
-public class MyNumber implements Expression
-{
-  private final int value;
+public abstract class MyNumber implements Expression {
 
-    /** getter method to obtain the value contained in the object
+    /**
+     * getter method to obtain the value contained in the object
      *
      * @return The integer number contained in the object
      */
-  public Integer getValue() { return value; }
+    public abstract Integer getIntegerValue();
 
-    /**
-     * Constructor method
-     *
-     * @param v The integer value to be contained in the object
-     */
-    public /*constructor*/ MyNumber(int v) {
-	  value=v;
-	  }
+    public abstract Double getRealValue();
 
     /**
      * accept method to implement the visitor design pattern to traverse arithmetic expressions.
      * Each number will pass itself to the visitor object to get processed by the visitor.
      *
-     * @param v	The visitor object
+     * @param v The visitor object
      */
-  public void accept(Visitor v) {
-      v.visit(this);
-  }
+    public void accept(Visitor v) {
+        v.visit(this);
+    }
 
 
-    /** The depth of a number expression is always 0
+    /**
+     * The depth of a number expression is always 0
      *
      * @return The depth of a number expression
      */
-  public int countDepth() {
-      CountVisitor v = new CountVisitor();
-      v.visit(this);
-      return v.getDepthCount();
-  }
+    public int countDepth() {
+        CountVisitor v = new CountVisitor();
+        v.visit(this);
+        return v.getDepthCount();
+    }
 
-    /** The number of operations contained in a number expression is always 0
+    /**
+     * The number of operations contained in a number expression is always 0
      *
      * @return The number of operations contained in a number expression
      */
-  public int countOps() {
-      CountVisitor v = new CountVisitor();
-      v.visit(this);
-      return v.getOpsCount();
-  }
+    public int countOps() {
+        CountVisitor v = new CountVisitor();
+        v.visit(this);
+        return v.getOpsCount();
+    }
 
-    /** The number of numbers contained in a number expression is always 1
+    /**
+     * The number of numbers contained in a number expression is always 1
      *
      * @return The number of numbers contained in  a number expression
      */
-  public int countNbs() {
-      CountVisitor v = new CountVisitor();
-      v.visit(this);
-      return v.getNbCount();
-  }
+    public int countNbs() {
+        CountVisitor v = new CountVisitor();
+        v.visit(this);
+        return v.getNbCount();
+    }
 
     /**
-     * Convert a number into a String to allow it to be printed.
+     * Two MyNumber expressions are equal if the values they contain are equal
      *
-     * @return	The String that is the result of the conversion.
+     * @param o The object to compare to
+     * @return A boolean representing the result of the equality test
      */
-  @Override
-  public String toString() {
-	  return Integer.toString(value);
-  }
+    @Override
+    public boolean equals(Object o) {
+        // No object should be equal to null (not including this check can result in an exception if a MyNumber is tested against null)
+        if (o == null) return false;
 
-  /** Two MyNumber expressions are equal if the values they contain are equal
-   *
-   * @param o The object to compare to
-   * @return  A boolean representing the result of the equality test
-   */
-  @Override
-  public boolean equals(Object o) {
-      // No object should be equal to null (not including this check can result in an exception if a MyNumber is tested against null)
-      if (o == null) return false;
+        // If the object is compared to itself then return true
+        if (o == this) {
+            return true;
+        }
 
-      // If the object is compared to itself then return true
-      if (o == this) {
-          return true;
-      }
+        //TODO Fix this method somehow
 
-      // If the object is of another type then return false
-      if (!(o instanceof MyNumber)) {
+        // If the object is of another type then return false
+        if (!(o instanceof MyNumber)) {
             return false;
-      }
-      return this.value == ((MyNumber)o).value;
-      // Used == since the contained value is a primitive value
-      // If it had been a Java object, .equals() would be needed
-  }
+        }
+        return Objects.equals(this.getRealValue(), ((MyNumber) o).getRealValue());
+        // Used == since the contained value is a primitive value
+        // If it had been a Java object, .equals() would be needed
 
-    /** The method hashCode needs to be overridden it the equals method is overridden;
-     * 	otherwise there may be problems when you use your object in hashed collections
-     * 	such as HashMap, HashSet, LinkedHashSet.
+    }
+
+    /**
+     * The method hashCode needs to be overridden it the equals method is overridden;
+     * otherwise there may be problems when you use your object in hashed collections
+     * such as HashMap, HashSet, LinkedHashSet.
      *
-     * @return	The result of computing the hash.
+     * @return The result of computing the hash.
      */
-  @Override
-  public int hashCode() {
-		return value;
-  }
+    @Override
+    public int hashCode() {
+        return getRealValue().hashCode();
+    }
 
 }
